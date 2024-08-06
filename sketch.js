0d44bfa1c86bf07f921c22f27f709b4ede160136
GadoSM <00001087328366sp@aluno.educacao.sp.gov.br>
2024-08-06 11:04:04 -0300

    modifiquei os codigos

--- sketch.js
+++ sketch.js
@@ -1,32 +1,34 @@
-let cor;
-let circuloX; // horizontal
-let circuloY; // vertical
-
+let cor
+ let posicaoHorizontal; // x
+ let posicaoVertical; // y
 function setup() {
-  createCanvas(400, 400);
-  background(color(100, 0 , 0));
-  cor = color(random(0, 255), random(0, 255), random(0, 255));
   
-  circuloX = [0, 0, 0];
-  circuloY = [random(height), random(height), random(height)];
-}
+  createCanvas(400, 400);
+  background('blue');
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+  }
 
 function draw() {
-  
-  fill(cor);
-  
-  for(let contador in circuloX) {
-    circle(circuloX[contador], circuloY[contador], 50);    
-    circuloX[contador]+= random(0,3);
-    circuloY[contador]+= random(-3,3); 
-    
-    if(circuloX[contador] >= width){
-      circuloX[contador] = 0;
-      circuloY[contador] = random(height);
-    }
+ fill(cor);
+ circle(posicaoHorizontal, posicaoVertical, 50);
+
+  if(mouseX < posicaoHorizontal) {
+  posicaoHorizontal = posicaoHorizontal - 1;
+  }
+  if(mouseX > posicaoHorizontal) {
+  posicaoHorizontal = posicaoHorizontal + 1;
+  } 
+  if(mouseY < posicaoVertical) {
+  posicaoVertical--;
+  }
+  if(mouseY > posicaoVertical) {
+  posicaoVertical++;
+  }
+  if(mouseIsPressed) {
+  cor = color(random(0, 255), random(0, 255), random(0, 255),
+  random(0, 100));
   }
   
-  if(mouseIsPressed){
-    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
-  }
 }
